23e6e3139d3e086ca3149fe71b9b6a42de8355ce
venom-10 <arohit0102@gmail.com>
2026-08-31 00:48:48 +0530

    refactor(metainfo): introduce TorrentFile API and remove legacy bencode package

--- internal/metainfo/metainfo.go
+++ internal/metainfo/metainfo.go
@@ -6,11 +6,30 @@ import (
 	"github.com/venom-10/torrent-client/internal/bencode"
 )
 
-func Open(filePath string) (*bencode.BencodeTorrent, error) {
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
+func Open(filePath string) (*TorrentFile, error) {
 	data, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
+	bt, err := bencode.Parse(data)
 
-	return bencode.Parse(data)
+	tf := &TorrentFile{
+		Announce:    bt.Announce,
+		InfoHash:    bt.InfoHash,
+		PieceHashes: bt.Info.Pieces,
+		PieceLength: bt.Info.PieceLength,
+		Length:      bt.Info.Length,
+		Name:        bt.Info.Name,
+	}
+
+	return tf, err
 }
